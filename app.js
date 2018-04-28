7db60ab5ee46aa8f006a7372e32e35c231910caf
slk007 <shubham.soni.lko@gmail.com>
2018-04-28 17:53:59 +0530

    removed gfs

--- app.js
+++ app.js
@@ -23,117 +23,16 @@ app.use(bodyParser.json());
 //methodOverride middleware
 app.use(methodOverride('_method'));
 
-//mongo URI
-const mongoURI = 'mongodb://slk007:slk007@ds131137.mlab.com:31137/gt_uploads'
 
-//create mongo connection
-const connect = mongoose.createConnection(mongoURI);
-
-//init GridFsStorage
-let gfs;
-connect.once('open', function () {
-  //init stream
-  gfs = Grid(connect.db, mongoose.mongo);
-  gfs.collection('assigments');
-})
-
-//create storage engine
-const storage = new GridFsStorage({
-  url: mongoURI,
-  file: (req, file) => {
-    return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err);
-        }
-
-        const filename = buf.toString('hex') + path.extname(file.originalname);
-        const fileInfo = {
-          filename: filename,
-          metadata: {
-            title: req.body.title,
-            due_date: req.body.due
-          },
-          bucketName: 'assigments'
-        };
-        resolve(fileInfo);
-      });
-    });
-  }
-});
-
-const upload = multer({ storage });
-
-
-
-//2nd collection--------------------------------------------------------------------------
-//init GridFsStorage
-let gfs2;
-connect.once('open', function () {
-  //init stream
-  gfs2 = Grid(connect.db, mongoose.mongo);
-  gfs2.collection('tests');
-})
-//
-// //create storage engine
-const storage2 = new GridFsStorage({
-  url: mongoURI,
-  file: (req, file) => {
-    return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err);
-        }
-        console.log(file);
-        const filename = buf.toString('hex') + path.extname(file.originalname);
-        const fileInfo = {
-          filename: filename,
-          // metadata: {
-          //   title: req.body.title,
-          //   test_date: req.body.test_date
-          // },
-          bucketName: 'tests'
-        };
-        resolve(fileInfo);
-      });
-    });
-  }
-});
-//
-const upload2 = multer({ storage2 });
-//---------------------------------------------------------------------
 
 //Home route
 app.get('/', (req, res) => {
   res.render('home')
 });
 
-app.get('/assigments', (req, res) => {
+app.get('/assigments');
 
-  gfs.files.find().toArray((err, files) => {
-
-    if(!files || files.length === 0){
-      res.render('index_assigment',{files: false});
-    } else {
-      res.render('index_assigment', {files: files});
-    }
-  })
-});
-
-// app.get('/tests', (req, res) => {
-//
-//   gfs2.files.find().toArray((err, files) => {
-//
-//     if(!files || files.length === 0){
-//       res.render('index_test',{files: false});
-//     } else {
-//       res.render('index_test', {files: files});
-//     }
-//   })
-//
-//   res.render('index_test')
-//
-// });
+app.get('/tests');
 
 app.get('/assigments/upload',(req, res) => {
   res.render('upload_assigment', {
@@ -147,16 +46,9 @@ app.get('/tests/upload',(req, res) => {
   });
 });
 
-app.post('/assigments/upload', upload.single('file'),(req, res) => {
-  // res.json({file: req.file});
-  res.redirect('/assigments')
-});
+app.post('/assigments/upload');
 
-app.post('/tests/upload', upload2.single('file'),(req, res) => {
-  // res.json({file: req.file});
-  // console.log(req);
-  res.redirect('/tests')
-});
+app.post('/tests/upload');
 
 //starting server
 app.listen(port, () => {
